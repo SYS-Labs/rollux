2747a454363c24659517da5967da9185eb5eab82
Abdul <giwa.omotola@gmail.com>
2023-03-21 18:14:17 +0000

    temporary changes

--- ops-bedrock/p2p-tanenbaum-up.sh
+++ ops-bedrock/p2p-tanenbaum-up.sh
@@ -58,10 +58,10 @@ function wait_up {
 }
 #mkdir -p ./.devnet
 
-# Export all secrets from file to environment variables
-(
-  export $(grep -v '^#' ./envs/op-node.env | xargs)
-)
+## Export all secrets from file to environment variables
+#(
+#  export $(grep -v '^#' ./envs/op-node.env | xargs)
+#)
 # Regenerate the L1 genesis file if necessary. The existence of the genesis
 # file is used to determine if we need to recreate the devnet's state folder.
 if [ ! -f "$DEVNET/done" ]; then
