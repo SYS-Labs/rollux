3a8e46659d69718dc09be979e53b2ccfd9ef023d
Abdul <giwa.omotola@gmail.com>
2023-04-01 19:16:08 +0100

    change address

--- packages/contracts-periphery/config/deploy/goerli.ts
+++ packages/contracts-periphery/config/deploy/goerli.ts
@@ -1,11 +1,11 @@
 import { DeployConfig } from '../../src'
 
 const config: DeployConfig = {
-  ddd: '0x9C6373dE60c2D3297b18A8f964618ac46E011B58',
-  l2ProxyOwnerAddress: '0x60c5C9c98bcBd0b0F2fD89B24c16e533BaA8CdA3',
+  ddd: '0x3071b919a37D3217fA19d54f8E1a08AEC4d8eB4e',
+  l2ProxyOwnerAddress: '0x48ab1cE92e1ea9713AdDeA668E146f575D60954e',
   optimistName: 'Optimist',
   optimistSymbol: 'OPTIMIST',
-  attestorAddress: '0x60c5C9c98bcBd0b0F2fD89B24c16e533BaA8CdA3',
+  attestorAddress: '0x48ab1cE92e1ea9713AdDeA668E146f575D60954e',
 }
 
 export default config
