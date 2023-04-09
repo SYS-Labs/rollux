ba044203730c54aa5481655c7b8ecfd590243f72
jagdeep sidhu <sidhujag@syscoin.org>
2023-04-09 13:50:22 -0700

    ensure start number cannot be >= not just = to prevent possible nil ptr

--- op-batcher/batcher/driver.go
+++ op-batcher/batcher/driver.go
@@ -203,7 +203,8 @@ func (l *BatchSubmitter) loadBlocksIntoState(ctx context.Context) {
 	if err != nil {
 		l.log.Warn("Error calculating L2 block range", "err", err)
 		return
-	} else if start.Number == end.Number {
+	// SYSCOIN
+	} else if start.Number >= end.Number {
 		return
 	}
 
