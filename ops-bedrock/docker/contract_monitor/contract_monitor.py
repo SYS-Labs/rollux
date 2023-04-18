0a66860d25e6894f2dd7b3e4d2f239d5e836ecf1
Abdul <giwa.omotola@gmail.com>
2023-04-18 13:22:43 +0100

    clean up

--- ops-bedrock/docker/contract_monitor/contract_monitor.py
+++ ops-bedrock/docker/contract_monitor/contract_monitor.py
@@ -11,14 +11,15 @@ logging.basicConfig(
   format='%(asctime)s [%(levelname)s] %(message)s',
   handlers=[logging.StreamHandler()]
 )
-L2_URL = os.getenv("L2_URL")
+L2_URL = os.getenv("L1_URL")
 PORT = 8000
-BATCH_INBOX_ADDRESS = os.getenv("BATCH_INBOX_ADDRESS")
+BATCH_INBOX_ADDRESS = os.getenv("CONTRACT_ADDRESS")
+METRIC_NAME = os.getenv("METRIC_NAME")
 
 w3 = Web3(Web3.HTTPProvider(L2_URL))
 contract_address = w3.to_checksum_address(BATCH_INBOX_ADDRESS)
 
-contract_health_metric = Gauge("batcher_health", "Contract called in the past hour", ["called_in_past_hour"])
+contract_health_metric = Gauge(METRIC_NAME, "Contract called in the past hour", ["called_in_past_hour"])
 
 
 def check_contract_called_in_past_hour():
@@ -28,7 +29,7 @@ def check_contract_called_in_past_hour():
 
     # Iterate back through blocks to find the one that is approximately two hours ago
     two_hours_ago_block_number = current_block_number
-    one_hour_ago_timestamp = current_timestamp - 3600
+    one_hour_ago_timestamp = current_timestamp - 7200
 
     while True:
         logging.info(f"Entering while loop for checking all blocks in the past 2hrs")
@@ -44,7 +45,7 @@ def check_contract_called_in_past_hour():
       'address': contract_address
     }
     logging.info(f"filter params: {filter_params}")
-    contract_call_events = w3.eth.get_logs(filter_params)
+    contract_call_events = w3.eth.get_filter_logs(filter_params)
     logging.info(f"number of contract call events is {len(contract_call_events)}")
     return len(contract_call_events) > 0
 
