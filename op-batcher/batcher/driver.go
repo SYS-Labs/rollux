0f37ca18caaadf010d9766e0560fc13736af4108
Abdul <giwa.omotola@gmail.com>
2024-02-21 17:16:13 +0000

    fix gas limit

--- op-batcher/batcher/driver.go
+++ op-batcher/batcher/driver.go
@@ -444,7 +444,7 @@ func (l *BatchSubmitter) blobTxCandidate(data []byte) (*txmgr.TxCandidate, error
 		To:    &l.RollupConfig.BatchInboxAddress,
 		Blobs: []*eth.Blob{&b},
 		// SYSCOIN let L1 estimate gas due to precompile
-		GasLimit: 40,
+		GasLimit: 0,
 	}, nil
 }
 
