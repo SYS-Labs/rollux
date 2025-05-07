2a21fd9553fd5c149b76141789128543e6981e73
Abdul <giwa.omotola@gmail.com>
2025-05-07 20:49:11 +0100

    show vars

--- packages/contracts-bedrock/scripts/DeployOutputOracle.sol
+++ packages/contracts-bedrock/scripts/DeployOutputOracle.sol
@@ -2,18 +2,19 @@
 pragma solidity 0.8.15;
 
 import "forge-std/Script.sol";
+import { Deployer } from "./Deployer.sol";
 import { L2OutputOracle } from "../contracts/L1/L2OutputOracle.sol";
 
 contract Deploy is Script {
     // Modify these values as needed.
-    uint256 internal constant SUBMISSION_INTERVAL = 1800;                 // e.g. ~ every 30 minutes
+    uint256 internal constant SUBMISSION_INTERVAL = 1500;                 // e.g. ~ every 30 minutes
     uint256 internal constant L2_BLOCK_TIME = 2;                          // e.g. 2 seconds
-    uint256 internal constant FINALIZATION_PERIOD_SECONDS = 864000;       // e.g. 10 days
+    uint256 internal constant FINALIZATION_PERIOD_SECONDS = 3600;       // e.g. 10 days
     uint256 internal constant L2_OUTPUT_ORACLE_STARTING_BLOCK_NUMBER = 0;
-    uint256  internal constant L2_OUTPUT_ORACLE_STARTING_TIMESTAMP = 0;
+    uint256  internal constant L2_OUTPUT_ORACLE_STARTING_TIMESTAMP = 1687365241;
 
-    address internal constant L2_OUTPUT_ORACLE_PROPOSER   = 0xF92BeC130533d9A791166902C29A6a7B6766BF2D;
-    address internal constant L2_OUTPUT_ORACLE_CHALLENGER = 0x5925c588F6ef60052Df686Eea67284968a54762F;
+    address internal constant L2_OUTPUT_ORACLE_PROPOSER   = 0x21821Dc24dd29D2D23fE7223dB2BB8D2a72cC95F;
+    address internal constant L2_OUTPUT_ORACLE_CHALLENGER = 0xEF64E089f2e018C2d71213d6bB402EC14ce2c189;
 
     function run() external {
         // Start Foundry "broadcast" to perform on-chain deployments.
